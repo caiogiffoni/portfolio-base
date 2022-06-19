67e33fdf93c924088e06ffbb1950cabc68303e29
Caio Giffoni <caio_cgf@hotmail.com>
2022-06-19 18:25:03 -0300

    :bug: fix description color

--- src/provider/Colors/index.tsx
+++ src/provider/Colors/index.tsx
@@ -15,7 +15,7 @@ export const ColorContext = createContext<ColorContextProps | null>(null);
 // criar o provider
 export const ColorProvider = ({ children }: any) => {
   const colorWordsStyle = useColorModeValue("blue.500", "green.400");
-  const colorWordsDesc = useColorModeValue("#262626", "#c4c3c3");
+  const colorWordsDesc = useColorModeValue("#262626", "#ddd");
   const bgColor1 = useColorModeValue("#e2e2e2", "#212121");
   const bgColor2 = useColorModeValue("#c8c8c8", "#262626");
 
