b281abaab43af07e60334ac0b872538c55d6cb9c
Caio Giffoni <caio_cgf@hotmail.com>
2022-09-28 10:19:31 -0300

    :bug: fix: adjusted tec card animation

--- src/components/Knowledge/Card/index.tsx
+++ src/components/Knowledge/Card/index.tsx
@@ -1,8 +1,11 @@
-import { Box, Image, Tooltip } from "@chakra-ui/react";
+import { Box, Tooltip } from "@chakra-ui/react";
 import { IconType } from "react-icons/lib";
 import { keyframes } from "@stitches/react";
 import { useColors } from "../../../provider/Colors";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useMediaQuery } from "@chakra-ui/react";
 
 export const scaleUp = keyframes({
   "0%": { transform: "translateY(15%)" },
@@ -11,20 +14,56 @@ export const scaleUp = keyframes({
 
 interface StackProps {
   title: string;
-  icon: string | IconType;
+  icon: IconType;
   key: number;
+  i: number;
 }
 
-export const CardKnowledge = (
-  { title, icon: Icon }: StackProps,
-  key: number
-): JSX.Element => {
+export const CardKnowledge = ({
+  title,
+  icon: Icon,
+  i,
+}: StackProps): JSX.Element => {
+  const [isLargerThan1280] = useMediaQuery("(min-width: 1280px)");
+  const boxVariant = {
+    initial: {
+      opacity: 0,
+      translateX: i % 2 === 0 ? -70 : 70,
+      translateY: -70,
+    },
+    visible: {
+      opacity: 1,
+      translateX: 0,
+      translateY: 0,
+      transition: {
+        duration: isLargerThan1280 ? i * 0.3 : 0.8,
+        delay: isLargerThan1280 ? i * 0.3 : 0.3,
+      },
+    },
+    hidden: { opacity: 0 },
+  };
+
+  const control = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("initial").then((res) => control.start("visible"));
+    } else {
+      control.start("hidden");
+    }
+  }, [control, inView]);
+
   const { colorWordsDesc } = useColors();
   const [isLabelOpen, setIsLabelOpen] = useState(false);
 
   return (
     <Tooltip label={title} isOpen={isLabelOpen}>
       <Box
+        as={motion.div}
+        animate={control}
+        ref={ref}
+        variants={boxVariant}
         minW="150px"
         minH="150px"
         sx={{
--- src/components/Knowledge/index.tsx
+++ src/components/Knowledge/index.tsx
@@ -1,25 +1,9 @@
 import { Box, Flex, Heading, HStack } from "@chakra-ui/react";
-
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
 import { useColors } from "../../provider/Colors";
 import { CardKnowledge } from "./Card";
 import { stackData } from "../../utils/stackData";
 
 export const Knowledge = () => {
-
-  const control = useAnimation();
-  const [ref, inView] = useInView();
-
-  useEffect(() => {
-    if (inView) {
-      control.start("initial").then((res) => control.start("visible"));
-    } else {
-      control.start("hidden");
-    }
-  }, [control, inView]);
-
   const { colorWordsDesc, bgColor2 } = useColors();
 
   return (
@@ -44,35 +28,13 @@ export const Knowledge = () => {
           >
             <Box display="flex" flexWrap="wrap" justifyContent="center" gap={5}>
               {stackData.map((stack, i) => {
-                const boxVariant = {
-                  initial: {
-                    opacity: 0,
-                    translateX: i % 2 === 0 ? -70 : 70,
-                    translateY: -70,
-                  },
-                  visible: {
-                    opacity: 1,
-                    translateX: 0,
-                    translateY: 0,
-                    transition: { duration: 0.7, delay: i * 0.3 },
-                  },
-                  hidden: { opacity: 0 },
-                };
-
                 return (
-                  <Box
-                    as={motion.div}
-                    animate={control}
-                    ref={ref}
-                    variants={boxVariant}
+                  <CardKnowledge
                     key={i}
-                  >
-                    <CardKnowledge
-                      key={i}
-                      title={stack.title}
-                      icon={stack.img}
-                    />
-                  </Box>
+                    title={stack.title}
+                    icon={stack.img}
+                    i={i}
+                  />
                 );
               })}
             </Box>
